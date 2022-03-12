8c61bcac8f79b163c209ea460545b5b66fa793ba
jacksonlmp <jacksonlmp17.jl@gmail.com>
2022-03-12 12:18:35 -0300

    Notebook

--- dados_pandas.py
+++ dados_pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #Leitura do arquivo
 notas = pd.read_csv("dados/ratings.csv")
@@ -6,6 +7,9 @@ notas = pd.read_csv("dados/ratings.csv")
 #Muda os nomes da coluna
 notas.columns = ["usuarioID", "filmeID", "nota", "momento"]
 
-print(notas['nota'])
-print("="*60)
-print(notas['nota'].unique())
+# print(notas['nota'])
+# print("="*60)
+# print(notas['nota'].unique())
+
+notas.nota.plot()
+plt.show()
